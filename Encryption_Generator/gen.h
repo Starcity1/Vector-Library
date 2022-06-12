b157e05202b856ecb454fee97edf3fb3d896aa7f
David_Rodriguez <david.rodriguez220443@gmail.com>
2022-06-11 20:46:07 -0500

    Started the encryption library

--- Encryption_Generator/gen.h
+++ Encryption_Generator/gen.h
@@ -7,6 +7,20 @@ A library that contains various encryption generators and able to produce variou
 will constantly update this library, please read the README.md file to learn more about this library.
 */
 
+/*
+TODO List:
+- Add ceasar cipher and decipher
+- Add substitution cipher and decipher
+- Add transposition cipher and decipher
+- Add ascii-character encryption and decryption
+- Add RSA encryption and decryption
+- Add AES encryption and decryption
+
+NOTE: All encryptions and decryptions methods will be stored on the same class generators.
+*/
+
+#include "Source/ceasar.cpp"
+
 #pragma once;
 
 namespace EZEncrypt {
